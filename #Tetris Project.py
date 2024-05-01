210d7e10f5f7c74d050ba4c7d7d11dd6c3fcde34
jbaker454 <165109655+jbaker454@users.noreply.github.com>
2024-05-01 10:09:04 -0500

    Add files via upload

--- #Tetris Project.py
+++ #Tetris Project.py
@@ -1,5 +1,7 @@
 #Tetris Project
 import tkinter as tk
+import threading
+import time
 
 window = tk.Tk()
 
@@ -33,50 +35,119 @@ for row in range(3):
 scoreLabel = tk.Label(master=menuFrame,bg="#ffffff",text="score: ",\
                        highlightthickness=2,highlightbackground="#000000",\
                        borderwidth=4)
-rotateLeftButton = tk.Button(master=menuFrame,bg="#1a1aff",\
+moveLeftButton = tk.Button(master=menuFrame,bg="#1a1aff",width=2,height=1,text="A",\
                               highlightthickness=2,highlightbackground="#000000",\
                               borderwidth=4)
-moveDownButton = tk.Button(master=menuFrame,bg="#1a1aff",\
+moveDownButton = tk.Button(master=menuFrame,bg="#1a1aff",width=2,height=1,text="S",\
                             highlightthickness=2,highlightbackground="#000000",\
                             borderwidth=4)
-rotateRightButton = tk.Button(master=menuFrame,bg="#1a1aff",\
+moveRightButton = tk.Button(master=menuFrame,bg="#1a1aff",width=2,height=1,text="D",\
                                highlightthickness=2,highlightbackground="#000000",\
                                borderwidth=4)
-pauseButton = tk.Button(master=menuFrame,bg="#1a1aff",\
+rotateButton = tk.Button(master=menuFrame,bg="#1a1aff",width=2,height=1,text="R",\
+                               highlightthickness=2,highlightbackground="#000000",\
+                               borderwidth=4)
+pauseButton = tk.Button(master=menuFrame,bg="#1a1aff",width=5,height=1,text="pause",\
                          highlightthickness=2,highlightbackground="#000000",\
                          borderwidth=4)
-exitButton = tk.Button(master=menuFrame,bg="#1a1aff",\
+exitButton = tk.Button(master=menuFrame,bg="#1a1aff",width=5,height=1,text="exit",\
                         highlightthickness=2,highlightbackground="#000000",\
                         borderwidth=4)
 
-print(scoreLabel.winfo_screenwidth())
-
-scoreLabel.place(relx=.5,x=-(scoreLabel.winfo_screenwidth() / 50), y=5)
-rotateLeftButton.place()
-moveDownButton.place()
-rotateRightButton.place()
-pauseButton.place()
-exitButton.place()
+scoreLabel.place(x=5, y=5)
+rotateButton.place(relx=.75,x=-5, y=100)
+moveLeftButton.place(relx=.25,x=-25, y=150)
+moveDownButton.place(relx=.50,x=-15, y=150)
+moveRightButton.place(relx=.75,x=-5, y=150)
+pauseButton.place(relx=.25,x=-25, y=200)
+exitButton.place(relx=.50,x=10, y=200)
 
 window.configure(height=550,width=650)
 
+class Field:
+    def __init__(self):
+        pass
+    
+    def checkRowCompletion():
+        return
+
+    def removeRow():
+        return
+
+class Block:
+    def __init__(self, color, name, tileConfig):
+        self.color = color
+        self.name = name
+        self.tileConfig = tileConfig
+    
+    def setBlock():
+        return
+
+    def preSetBlock():
+        return
+
+    def setNewBlock(self):
+        self.setBlock()
+        self.preSetBlock()
+        return
+
+    def checkBlockDown():
+        return
+
+    def moveBlockDown():
+        return
+    
+    def makeTilesStatic():
+        return
+
 class Tile:
-    def __init__(self, location, frame, color, dynamic = True):
+    def __init__(self, location, frame, dynamic = True):
         self.location = location
         self.frame = frame
-        self.color = color
         self.dynamic = dynamic
 
 def setupGUI ():
     boardFrame.place(x=0,y=0)
     queFrame.place(x=450,y=25)
     menuFrame.place(x=450,y=200)
-
-def startGame (event):
-    startFrame.destroy()
-    setupGUI()
     return
 
-startButton.bind("<Button-1>", startGame)
+def blockDown():
+    movable = Block.checkBlockDown()
+    if movable:
+        Block.moveBlockDown()
+    else:
+        rowComplete = Field.checkRowCompletion()
+        if rowComplete:
+            Field.removeRow()
+        Block.makeTilesStatic()
+        Block.setNewBlock
+        print("move down")
+    return movable
+
+def blockDownRepeat (event):
+    movable = True
+    while movable:
+        movable = blockDown()
+    return
+
+def blockLeft():
+    return
+
+def blockRight():
+    return
+
+def blockRotate():
+    return
+
+def newGame (event):
+    startFrame.destroy()
+    setupGUI()
+    Block.preSetBlock()
+    threading.Timer(function=blockDown(),interval=2)
+
+
+startButton.bind("<Button-1>", newGame)
+moveDownButton.bind("<Button-1>", blockDownRepeat)
 
 window.mainloop()
